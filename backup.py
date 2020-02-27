ea7d7e48a83b8e5a38be407a8f6ee0fd4a39c343
Kelvinmocrack <56204865+Kelvinmocrack@users.noreply.github.com>
2020-02-26 21:14:15 -0500

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,12 +1,14 @@
 #Nocturnalnetworking.com
 #
-#from netmiko import ConnectHandler
+from netmiko import ConnectHandler
+from getpass import getpass
+ask = getpass()
 
 SW1 = {
         'device_type': 'cisco_ios',
         'ip': '192.168.122.51',
         'username': 'kelvin',
-        'password': 'cisco',
+        'password': ask,
 
 }
 
@@ -14,28 +16,28 @@ SW2 = {
         'device_type': 'cisco_ios',
         'ip': '192.168.122.52',
         'username': 'kelvin',
-        'password': 'cisco',
+        'password': ask,
 }
 
 SW3 = {
         'device_type': 'cisco_ios',
         'ip': '192.168.122.53',
         'username': 'kelvin',
-        'password': 'cisco',
+        'password': ask,
 }
 
 SW4 = {
         'device_type': 'cisco_ios',
         'ip': '192.168.122.54',
         'username': 'kelvin',
-        'password': 'cisco',
+        'password': ask,
 }
 
 SW5 = {
         'device_type': 'cisco_ios',
         'ip': '192.168.122.55',
         'username': 'kelvin',
-        'password': 'cisco',
+        'password': ask,
 }
 
 switches = [[SW1, 'SW1'], [SW2, 'SW2'], [SW3, 'SW3'],[SW4, 'SW4'], [SW5,'SW5']]
